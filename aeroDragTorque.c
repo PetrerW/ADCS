2ab5fda010a20e9f1f63465098117e28adf3d4b6
PetrerW <piotrw76@gmail.com>
2016-07-05 21:06:12 +0200

    Update aeroDragTorque.c

--- aeroDragTorque.c
+++ aeroDragTorque.c
@@ -1,75 +1,75 @@
 #include "Mathematics.h"
-#include <stdio.h>
-
-matrix3x3 skew(matrix3x1 w)
+//ECI - Earth Centered Inertial
+double aeroDragTorque(matrix r_eci, double v_eci, matrix q, double face, double R_z) //I don't know global value R_z so i'll set this as parameter
 {
-	matrix3x3 A;
-	int i, j;
-	for (i = 0; i < 3; i++) //A = zeros(3)
-		for (j = 0; j < 3; j++)
-			A.m_data[i][j] = 0;
+	double w_ez = 7.2921158553e-5; //[rad/s]
+	matrix w_e;
+	w_e.q[0] = (double* )malloc(sizeof(double) * 3); 
+	w_e.q[0][0] = w_ez * 1;	//First row, first position
+	w_e.q[0][1] = 0;	//First row, second position
+	w_e.q[0][2] = 0;	//First row, third position
 
-	//Assigning values so that A is skew-symmetric matrix3x3
-	A.m_data[1][2] = -*(w.m_data[3]);
-	A.m_data[1][3] = *(w.m_data[2]);
+	//w_e looks now like: [0 0 1]' (transposed)
 
-	A.m_data[2][1] = *(w.m_data[3]);
-	A.m_data[2][3] = -*(w.m_data[1]); 
+	double w_e[3] = { w_ez * 1, 0, 0 }; //earth rate vector in eci
+	matrix *A;
+	A->length = 3;
+	A->width = 3;
+	*A = *skew(w_e);
+	matrix vi_rel;// = v_eci - (*A) * r_eci; //relative satellite velocity to the atmosphere in eci [km/s]
 
-	A.m_data[3][1] = -*(w.m_data[2]);
-	A.m_data[3][2] = *(w.m_data[1]);
+	vi_rel.q = (double **)malloc(3 * sizeof(double));
+	int i;
+	for (i = 0; i < 3; i++) //Multiplying matrice 3x3 (A) and vector (1x3)
+	{
+		vi_rel.q[i][0] = (A->q[i][0] + A->q[i][1] + A->q[i][2]) * (w_e.q[i][0]);
+	}
+	vi_rel.length = 3;
+	vi_rel.width = 1;
+	//vi_rel: [x y z]'
+	
+	//GO ON HERE finish A_i2s and q2m
 
-	return A;
-}
+	double A_i2s = q2m(q);
+	double vs_rel = A_i2s * vi_rel; // relative velocity in satellite frame[km / s]
 
-double vectorNorm(matrix3x3 _r)  //Mocked it - Pablo has wrtitten the function
-{
-	return 1;
-}
+	double alt = vectorNorm(r_eci) - R_z; //approx altitude above spherical earth[km]
 
-double vectorNorm(matrix4x1 _r)
-{
-	return 0.0;
-}
-
-double vectorNorm(matrix3x1 _r)
-{
-	return 0.0;
-}
-
-matrix3x3 * q2m(matrix4x1 Q)
-{
-	// Calculating rotation matrix3x3 from quaternion[q1 q2 q3 q4]' where q4 is
-	// scalar part
-	/*
-	function A = q2m(q)
-		A = (q(4) ^ 2 - vectorNorm(q(1:3)) ^ 2) * eye(3) - 2 * q(4) * skew(q(1:3)) + 2 * q(1:3) * q(1:3)';
-		end
-	*/
-	matrix3x3 eye_3 = { //3x3 identity matrix
+	double ro0, H, h0;
+		if (alt >= 450 && alt < 500)
 		{
-			{1, 0, 0},
-			{0, 1, 0},
-			{0, 0, 1}
+			ro0 = 1.585e-12;
+			H = 62.2;
+			h0 = 450;
 		}
-	};
 
+		else if (alt >= 500 && alt < 600)
+		{
+			ro0 = 6.967e-13;
+			H = 65.8;
+			h0 = 500;
+		}
 
-	matrix3x3 A;
-	A = ( (Q.m_data[3])*(Q.m_data[3]) ) //START HERE
+		else if (alt >= 600 && alt < 700)
+		{
+			ro0 = 1.454e-13;
+			H = 79;
+			h0 = 600;
+		}
 
+		else if (alt >= 700 && alt <= 800)
+		{
+			ro0 = 3.614e-14;
+			H = 109;
+			h0 = 700;
+		}
+		else
+			printf("Altitude only in the range 450 - 800 km \n");
 
-//Multiplying quaternions: http://www.mathworks.com/help/aerotbx/ug/quatmultiply.html
-//I = eye(n) returns an n - by - n identity matrix3x33x3 with ones on the main diagonal and zeros elsewhere.
+		double ro = ro0 * exp((h0 - alt) / H); //Calculating atmosphere dencity
+		double cd = 2.2; // drag coeff
 
-	return 0;
+		double aero_torque = 0;
+
 }
-
-matrix4x1 q_to_2nd_power(matrix4x1 Q)
-{
-
-	return 0;
-}
-
-
 
