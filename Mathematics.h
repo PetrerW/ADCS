1680653dfe347fd32e93d5e5d851c772fdae55b9
PetrerW <piotrw76@gmail.com>
2016-07-10 00:35:22 +0200

    Update Mathematics.h

--- Mathematics.h
+++ Mathematics.h
@@ -11,12 +11,23 @@ typedef struct matrix3x1Tag {
 	double m_data[3];
 }matrix3x1;
 
+typedef struct matrix1x3Tag {
+	double m_data[3];
+}matrix1x3;
+
+
+//3x1 : [1 2 3] three columns, one row
+//1x3:  [1] one column, three rows
+//		[2]
+//		[3]
 //Convention: q[0][1] is first row (0) and second column (1)
 
-matrix3x3 skew(matrix3x1 w);
+matrix3x3 skew3x1(matrix3x1 w);
+matrix3x3 skew1x3(matrix1x3 w);
 double vectorNorm3x3(matrix3x3 _r);
 double vectorNorm4x1(matrix4x1 _r);
 double vectorNorm3x1(matrix3x1 _r);
+double vectorNorm1x3(matrix1x3 _r);
 matrix3x3 q2m(matrix4x1 Q);
 
 
