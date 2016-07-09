733dbf1ff300f0f04d53a272b7167e274b0a48b2
PetrerW <piotrw76@gmail.com>
2016-07-10 00:36:29 +0200

    Update Mathematics.c

--- Mathematics.c
+++ Mathematics.c
@@ -1,8 +1,8 @@
 #include "Mathematics.h"
 #include <stdio.h>
-#define FIRST 1
+#define SINGLE_NUMBER 0
 
-matrix3x3 skew(matrix3x1 w)
+matrix3x3 skew3x1(matrix3x1 w)
 {
 	matrix3x3 A;
 
@@ -21,17 +21,42 @@ matrix3x3 skew(matrix3x1 w)
 	return A;
 }
 
+matrix3x3 skew1x3(matrix1x3 w) //Similar with skew(matrix3x1 w) funciton, because it actually does the same thing, 
+							//just the vector is transposed
+{
+	matrix3x3 A;
+
+	//Assigning values so that A is skew-symmetric matrix3x3
+	A.m_data[0][1] = -w.m_data[3];
+	A.m_data[0][2] = w.m_data[2];
+
+	A.m_data[1][0] = w.m_data[3];
+	A.m_data[1][2] = -w.m_data[1];
+
+	A.m_data[2][0] = -w.m_data[2];
+	A.m_data[2][1] = w.m_data[1];
+
+	A.m_data[0][0] = A.m_data[1][1] = A.m_data[2][2] = 0; //Rest of fields in matrix are zeros.
+
+	return A;
+}
+
 double vectorNorm3x3(matrix3x3 _r)  //Mocked it - Pablo has wrtitten the function
 {
 	return 1;
 }
 
-double vectorNorm4x1(matrix4x1 _r)
+double vectorNorm4x1(matrix4x1 _r) //Mocked it - Pablo has wrtitten the function
 {
 	return 0.0;
 }
 
-double vectorNorm3x1(matrix3x1 _r)
+double vectorNorm3x1(matrix3x1 _r) //Mocked it - Pablo has wrtitten the function
+{
+	return 0.0;
+}
+
+double vectorNorm1x3(matrix1x3 _r) //Mocked it - Pablo has wrtitten the function
 {
 	return 0.0;
 }
@@ -45,13 +70,6 @@ matrix3x3 q2m(matrix4x1 Q)
 		A = (q(4) ^ 2 - vectorNorm(q(1:3)) ^ 2) * eye(3) - 2 * q(4) * skew(q(1:3)) + 2 * q(1:3) * q(1:3)';
 		end
 	*/
-	matrix3x3 eye_3 = { //3x3 identity matrix
-		{
-			{1, 0, 0},
-			{0, 1, 0},
-			{0, 0, 1}
-		}
-	};
 
 	matrix3x1 q1_3; //Instead of q(1:3)
 	matrix3x3 A; //It will be returned by the function
@@ -68,14 +86,14 @@ matrix3x3 q2m(matrix4x1 Q)
 		}
 	};
 	//=============================================================================================================
-	matrix3x1 q1_3_multiplied; //every component of q1_3_multiplied will be equal to q1_3 multiplied by Q.m_data[3]*2 
-								//in Matlab: q(4) * 2
-	for ( i = 0; i < 3; i++)
-		q1_3_multiplied.m_data[i] *= Q.m_data[3];
+	matrix3x1 q1_3_multiplied; //every component of q1_3_multiplied will be equal to q1_3 multiplied by Q.m_data[3]
+								//in Matlab: q(4)
+	for (i = 0; i < 3; i++)
+		q1_3_multiplied.m_data[i] = Q.m_data[3] * q1_3.m_data[i];
 
-	matrix3x3 A_2nd = skew(q1_3_multiplied); //Second part of the formula
+	matrix3x3 A_2nd = skew3x1(q1_3_multiplied); //Second part of the formula
 	//=============================================================================================================
-#if FIRST == 1
+#if SINGLE_NUMBER == 1
 	matrix3x3 A_3rd = { //Third part of the formula: q(1:3) * q(1:3)'
 		{ //It's multiplication of vectors 1x3 and 3x1, product is 3x3. If we multiply 3x1 by 1x3, we get a single number.
 			//I don't know which version is correct, but I assumed the first one.
@@ -97,7 +115,7 @@ matrix3x3 q2m(matrix4x1 Q)
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
 			A.m_data[i][j] = A_1st.m_data[i][j] - 2 * A_2nd.m_data[i][j] + 2 * a3;
-#endif //FIRST
+#endif //SINGLE_NUMBER
 
 
 //Multiplying quaternions: http://www.mathworks.com/help/aerotbx/ug/quatmultiply.html
