ddb09940cba55a703cd7784e0e39b061edc0d4f5
PetrerW <piotrw76@gmail.com>
2016-08-03 22:16:36 +0200

    Update Mathematics.h

--- Mathematics.h
+++ Mathematics.h
@@ -5,16 +5,19 @@ typedef struct matrix3x3Tag {
 
 typedef struct matrix4x1Tag {
 	double m_data[4];
-}matrix4x1;
+}matrix4x1; 
 
 typedef struct matrix3x1Tag {
 	double m_data[3];
-}matrix3x1;
+}matrix3x1; //3 columns, 1 row
 
 typedef struct matrix1x3Tag {
 	double m_data[3];
-}matrix1x3;
+}matrix1x3; //1 column, 3 rows
 
+typedef struct matrix6x3Tag {
+	double m_data[3][6]; //6 columns, 3 rows
+}matrix6x3;
 
 //3x1 : [1 2 3] three columns, one row
 //1x3:  [1] one column, three rows
@@ -30,6 +33,6 @@ double vectorNorm3x1(matrix3x1 _r);
 double vectorNorm1x3(matrix1x3 _r);
 matrix3x3 q2m(matrix4x1 Q);
 matrix4x1 m2q(matrix3x3 A);
-void quicksort(int *table, int left, int right);
+void quicksort(double table[], int left, int right);
 
 
