03e9e53fe8e1e1a1220cb9c0d60a1967c52ec563
PetrerW <piotrw76@gmail.com>
2016-08-31 15:59:10 +0200

    Update aeroDragTorque.c

--- aeroDragTorque.c
+++ aeroDragTorque.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include "FaceDiv.h"
 //ECI - Earth Centered Inertial
-double aeroDragTorque(matrix1x3 r_eci, matrix1x3 v_eci, matrix4x1 q, struct faceTag face, double R_z) //I don't know global value R_z so i'll set this as parameter
+matrix1x3 aeroDragTorque(matrix1x3 r_eci, matrix1x3 v_eci, matrix4x1 q, struct faceTag face, double R_z) //I don't know global value R_z so i'll set this as parameter
 {
 	int i, j;
 	double w_ez = 7.2921158553e-5; //[rad/s]
@@ -11,6 +11,7 @@ double aeroDragTorque(matrix1x3 r_eci, matrix1x3 v_eci, matrix4x1 q, struct face
 		{ 0, 0, w_ez }
 	}; 	//w_e looks like: [0 0 w_ez]' (transposed)
 
+	
 	matrix3x3 A = skew1x3(w_e);
 
 	matrix1x3 vi_rel, second_component; //relative satellite velocity to the atmosphere in eci [km/s]
@@ -18,7 +19,10 @@ double aeroDragTorque(matrix1x3 r_eci, matrix1x3 v_eci, matrix4x1 q, struct face
 											//whole equation is: vi_rel = v_eci - skew(w_e) * r_eci; 
 
 	matrix3x3 A_i2s = q2m(q);
-	matrix1x3 vs_rel;
+	matrix1x3 vs_rel, aero_torque_component[6], aero_torque;
+	
+	for(j = 0; j<3; j++)
+		aero_torque.m_data[j] = 0;
 
 	for (i = 0; i < 3; i++)
 	{//Calculating the second component (multiplying matrix A (3x3) by r_eci (1x3) gives 1x3 too
@@ -65,30 +69,40 @@ double aeroDragTorque(matrix1x3 r_eci, matrix1x3 v_eci, matrix4x1 q, struct face
 		double ro = ro0 * exp((h0 - alt) / H); //Calculating atmosphere dencity
 		double cd = 2.2; // drag coeff
 
-		double aero_torque = 0;
 		int face_s_legth = 6;
 
 		int lenght_face_s = 6;
-		double cos[6], aero__torque[6];
-		matrix6x3 force;
+		double cos[6], aero_torque[6];
+
+		matrix1x3 cp_column[6];
 		for (i = 0; i < lenght_face_s; i++)
 		{
 			//cos[i]= face.v(:, i)' * vs_rel / vectorNorm(vs_rel);
 			cos[i] = face.v[0][0] * vs_rel.m_data[0] + face.v[1][0] * vs_rel.m_data[1] + face.v[2][0] * vs_rel.m_data[2];
 			cos[i] /= vectorNorm1x3(vs_rel);
 			//force_i = - 0.5 * ro * cd * vectorNorm(vs_rel) * vs_rel * face.s(i) * max(cos_i, 0) * 1e6; % [N]
+			matrix1x3 force;
+			matrix1x3 cp_column;
 			for (j = 0; j < 3; j++) 
 			{
-				force.m_data[j][i] = -0.5*ro*cd*vectorNorm(vs_rel)*face.s[i] * max(cos[i], 0) * 1e6 * vs_rel.m_data[j];
+				force.m_data[j] = -0.5*ro*cd*vectorNorm(vs_rel)*face.s[i] * max(cos[i], 0) * 1e6 * vs_rel.m_data[j];
+
+				cp_column.m_data[j] = face.cp[j][i];	
 			}
-			//GO ON HERE
-			//GO ON HERE
-			//GO ON HERE
-			//GO ON HERE
-			//GO ON HERE
+			matrix3x3 skew_face_cp = skew(cp_column);
+			//aero_torque_i = skew(face.cp(:,i)) * force_i;
+			for (j=0 ; j < 3; j++)
+			{
+				//multiplying 3x3 by 1x3 (column)
+				aero_torque_component[i].m_data[j] = skew_face_cp.m_data[j][0] * force.m_data[0] + 
+					skew_face_cp.m_data[j][1] * force.m_data[1] + skew_face_cp.m_data[j][2] * force.m_data[2];
+			}
+			
+			for (j=0 ; j < 3; j++)
+				aero_torque.m_data[j] + = aero_torque_component[i].m_data[j];
 		}
 
 
-		return 0.0;
+		return aero_torque;
 }
 
